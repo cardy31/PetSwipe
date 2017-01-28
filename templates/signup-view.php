42fd1f8594a7c39626d64734cef200d2ba3d6d3b
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-28 16:35:25 -0500

    Form fixes

--- templates/signup-view.php
+++ templates/signup-view.php
@@ -1,43 +1,33 @@
 <div id="signup-form">
-    <form class="form-horizontal">
+    <form action="../public_html/new_member.php">
         <div class="form-group">
-            <label for="FirstName" class="col-2 col-form-label">First Name</label>
-            <div class="col-10">
-                <input class="form-control" type="text" placeholder="John" id="FName">
-            </div>
+            <label for="FirstName">First Name</label>
+            <input class="form-control" type="text" placeholder="John" id="FName">
         </div>
-        <div class="form-group row">
-            <label for="Last Name" class="col-2 col-form-label">Last Name</label>
-            <div class="col-10">
-                <input class="form-control" type="text" placeholder="Doe" id="LName">
-            </div>
+        <div class="form-group">
+            <label for="Last Name">Last Name</label>
+            <input class="form-control" type="text" placeholder="Doe" id="LName">
         </div>
+        <div class="form-group">
+            <label for="Email">Email</label>
+            <input class="form-control" type="email" placeholder="getapet@something.ca" id="Email">
         </div>
-        <div class="form-group row">
-            <label for="Email" class="col-2 col-form-label">Email</label>
-            <div class="col-10">
-                <input class="form-control" type="email" placeholder="getapet@something.ca" id="Email">
-            </div>
+        <div class="form-group">
+            <label for="birthdate">Birthdate</label>
+            <input class="form-control" type="date" placeholder="1900-01-01" id="Birthdate">
         </div>
+        <div class="form-group">
+            <label for="Address">Home Address</label>
+            <input class="form-control" type="text" placeholder="123 Anywhere Street" id="Address">
         </div>
-        <div class="form-group row">
-            <label for="birthdate" class="col-2 col-form-label">Birthdate</label>
-            <div class="col-10">
-                <input class="form-control" type="date" placeholder="1900-01-01" id="Birthdate">
-            </div>
+        <div class="form-group">
+            <label for="">Province</label>
+            <input class="form-control" type="text" placeholder="ON" id="Province">
         </div>
-        <div class="form-group row">
-            <label for="Address" class="col-2 col-form-label">Home Address</label>
-            <div class="col-10">
-                <input class="form-control" type="text" placeholder="123 Anywhere Street" id="Address">
-            </div>
+        <div class="form-group">
+            <label for="">Province</label>
+            <input class="form-control" type="text" placeholder="Zip Code" id="Zip">
         </div>
-        <div class="form-group row">
-            <label for="" class="col-2 col-form-label">Province</label>
-            <div class="col-10">
-                <input class="form-control" type="text" placeholder="ON" id="Province">
-            </div>
-        </div>
-
+        <button type="submit" class="btn btn-default">Submit</button>
     </form>
 </div>
