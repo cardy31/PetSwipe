cf9d5baeaae278267a45f9f481a029e4438707be
hlgreer <hannahgreer@rogers.com>
2017-01-28 17:58:53 -0500

    First update w/ sign up page

---
+++ public_html/new_member.php
@@ -0,0 +1,19 @@
+<?php
+
+$newMemberData = array();
+
+$newMemberData['Firstname'] = $_POST['first'];
+$newMemberData['Lastname'] = $_POST['last'];
+$newMemberData['Username'] = $_POST['username'];
+$newMemberData['Hash'] = crypt($_POST['password']);
+$newMemberData['Email'] = $_POST['email'];
+$newMemberData['Age'] = $_POST['age'];
+$newMemberData['Address'] = $_POST['address'];
+$newMemberData['Prov'] = $_POST['province'];
+$newMemberData['Postal'] = $_POST['postal'];
+$newMemberData['AnimalToAdopt'] = $_POST['pet'];
+
+$url = 'http://robcardy.com/API/';
+
+$ch = curl_init($url);
+
