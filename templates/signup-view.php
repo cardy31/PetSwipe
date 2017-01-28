9a35ba35471fc5b9de50532f4cd569e8e0199052
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-28 17:45:23 -0500

    Form fixes

--- templates/signup-view.php
+++ templates/signup-view.php
@@ -2,43 +2,52 @@
     <form action="../public_html/new_member.php">
         <div class="form-group">
             <label for="Firstname">First Name</label>
-            <input class="form-control" type="text" placeholder="John" id="first">
+            <input class="form-control" type="text" placeholder="John" name="first">
         </div>
         <div class="form-group">
             <label for="Lastname">Last Name</label>
-            <input class="form-control" type="text" placeholder="Doe" id="last">
+            <input class="form-control" type="text" placeholder="Doe" name="last">
         </div>
         <div class="form-group">
             <label for="Username">Username</label>
-            <input class="form-control" type="text" placeholder="Doe" id="username">
+            <input class="form-control" type="text" placeholder="Username" name="username">
         </div>
         <div class="form-group">
             <label for="Password">Password</label>
-            <input class="form-control" type="text" placeholder="Doe" id="password">
+            <input class="form-control" type="password" name="password">
         </div>
         <div class="form-group">
             <label for="Email">Email</label>
-            <input class="form-control" type="email" placeholder="john@doe.com" id="email">
+            <input class="form-control" type="email" placeholder="john@doe.com" name="email">
         </div>
         <div class="form-group">
             <label for="Age">Age</label>
-            <input class="form-control" type="date" placeholder="25" id="age">
+            <input class="form-control" type="date" placeholder="25" name="age">
         </div>
         <div class="form-group">
             <label for="Address">Address</label>
-            <input class="form-control" type="text" placeholder="123 Anywhere Street" id="address">
+            <input class="form-control" type="text" placeholder="123 Anywhere Street" name="address">
         </div>
         <div class="form-group">
             <label for="Province">Province</label>
-            <input class="form-control" type="text" placeholder="ON" id="province">
+            <input class="form-control" type="text" placeholder="ON" name="province">
         </div>
         <div class="form-group">
             <label for="Postal">Postal Code</label>
-            <input class="form-control" type="text" placeholder="Zip Code" id="postal">
+            <input class="form-control" type="text" placeholder="Zip Code" name="postal">
+        </div>
+        <div class="form-group">
+            <label for="Pet">I would like to adopt a (Dog, Cat, Rabbit)</label>
+            <input class="form-control" type="text" placeholder="Dog" name="pet">
         </div>
         <div class="checkbox">
             <label>
-                <input type="checkbox"> I'm open to adopting a pet with medical issues.
+                <input type="checkbox" name="pet"> I'm open to adopting a pet with medical issues.
+            </label>
+        </div>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name=""> I have owned a pet before.
             </label>
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
