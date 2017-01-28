d4c80b1eb7efb2550efd311e0a73706704a2b438
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-28 16:16:40 -0500

    Form fixes

--- templates/signup-view.php
+++ templates/signup-view.php
@@ -3,41 +3,41 @@
         <div class="form-group">
             <label for="FirstName" class="col-2 col-form-label">First Name</label>
             <div class="col-10">
-                <input class="form-control" type="text" value="John" id="FName">
+                <input class="form-control" type="text" placeholder="John" id="FName">
             </div>
         </div>
         <div class="form-group row">
             <label for="Last Name" class="col-2 col-form-label">Last Name</label>
             <div class="col-10">
-                <input class="form-control" type="text" value="Doe" id="LName">
+                <input class="form-control" type="text" placeholder="Doe" id="LName">
             </div>
         </div>
         </div>
         <div class="form-group row">
             <label for="Email" class="col-2 col-form-label">Email</label>
             <div class="col-10">
-                <input class="form-control" type="email" value="getapet@something.ca" id="Email">
+                <input class="form-control" type="email" placeholder="getapet@something.ca" id="Email">
             </div>
         </div>
         </div>
         <div class="form-group row">
             <label for="birthdate" class="col-2 col-form-label">Birthdate</label>
             <div class="col-10">
-                <input class="form-control" type="date" value="1900-01-01" id="Birthdate">
+                <input class="form-control" type="date" placeholder="1900-01-01" id="Birthdate">
             </div>
         </div>
         <div class="form-group row">
             <label for="Address" class="col-2 col-form-label">Home Address</label>
             <div class="col-10">
-                <input class="form-control" type="text" value="123 Anywhere Street" id="Address">
+                <input class="form-control" type="text" placeholder="123 Anywhere Street" id="Address">
             </div>
         </div>
         <div class="form-group row">
             <label for="" class="col-2 col-form-label">Province</label>
             <div class="col-10">
-                <input class="form-control" type="text" value="ON" id="Province">
+                <input class="form-control" type="text" placeholder="ON" id="Province">
             </div>
         </div>
-        
+
     </form>
 </div>
