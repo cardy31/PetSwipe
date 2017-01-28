f9f27594cd9903f734a8e32e658ea0800334fe5c
hlgreer <hannahgreer@rogers.com>
2017-01-28 15:58:48 -0500

    First update w/ sign up page

--- templates/home.php
+++ templates/home.php
@@ -1,3 +1,13 @@
 <h1>
-    Hello, world!
+    PawSwipes
 </h1>
+
+<button type = "button" class = "btn btn-primary" id="tosignup">Sign Up</button>
+<script>
+    $(function(){
+        $('#tosignup').click(function(){
+            window.location='signup.php'
+        });
+    });
+</script>
+
---
+++ templates/signup-view.php
@@ -0,0 +1,43 @@
+
+<form>
+    <div class="form-group">
+        <label for="FirstName" class="col-2 col-form-label">First Name</label>
+        <div class="col-10">
+            <input class="form-control" type="text" value="John" id="FName">
+        </div>
+    </div>
+    </div>
+    <div class="form-group row">
+        <label for="Last Name" class="col-2 col-form-label">Last Name</label>
+        <div class="col-10">
+            <input class="form-control" type="text" value="Doe" id="LName">
+        </div>
+    </div>
+    </div>
+    <div class="form-group row">
+        <label for="Email" class="col-2 col-form-label">Email</label>
+        <div class="col-10">
+            <input class="form-control" type="email" value="getapet@something.ca" id="Email">
+        </div>
+    </div>
+    </div>
+    <div class="form-group row">
+        <label for="birthdate" class="col-2 col-form-label">Birthdate</label>
+        <div class="col-10">
+            <input class="form-control" type="date" value="1900-01-01" id="Birthdate">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="Address" class="col-2 col-form-label">Home Address</label>
+        <div class="col-10">
+            <input class="form-control" type="text" value="123 Anywhere Street" id="Address">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="" class="col-2 col-form-label">Province</label>
+        <div class="col-10">
+            <input class="form-control" type="text" value="ON" id="Province">
+        </div>
+    </div>
+
+</form>
