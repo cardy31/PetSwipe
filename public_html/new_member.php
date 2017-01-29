667aa719f16c7f6c96bcacb086286e103ff6c47b
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-28 20:03:37 -0500

    Password change

--- public_html/new_member.php
+++ public_html/new_member.php
@@ -22,15 +22,15 @@ $json = json_encode($newMemberData);
 $url = 'http://www.robcardy.com/api/member/';
 
 
-$ch2 = curl_init($url);
-curl_setopt($ch2, CURLOPT_URL, $url);
-curl_setopt($ch2,CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch2, CURLOPT_USERPWD, "cardy31" . ":" . "JA83nq&E");
-curl_setopt($ch2, CURLOPT_POST, 1);
-curl_setopt($ch2, CURLOPT_POSTFIELDS, $newMemberData);
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-$data = curl_exec($ch2);
-curl_close($ch2);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_USERPWD, "default" . ":" . "password123");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $newMemberData);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$data = curl_exec($ch);
+curl_close($ch);
 
 var_dump($data);
 print_r(json_decode($data, true));
