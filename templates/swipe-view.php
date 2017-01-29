77302e11a9d9fcde65f4f55b9dd8163df523a003
hlgreer <hannahgreer@rogers.com>
2017-01-28 23:33:32 -0500

    Addition of skeleton swipe pages

--- templates/swipe-view.php
+++ templates/swipe-view.php
@@ -1,16 +1,33 @@
-<div class="col-4">
-        <img src="http://i.imgur.com/NoDketJ.jpg">
+
+<div class="col-sm-4">
+    <button type="submit" class="btn btn-danger">Bad</button>
 </div>
 
-<div class="col-4">
+<div class="col-sm-4">
     <img src="http://i.imgur.com/NoDketJ.jpg">
 </div>
 
-<div class="col-4">
-    <img src="http://i.imgur.com/NoDketJ.jpg">
+<div class="col-sm-4">
+    <button type="submit" class="btn btn-submit">Good</button>
+
 </div>
 
-
+<script>
+    document.onkeydown = function(e) {
+        if (e.keyCode == '37') {
+            // left
+            $.post("submitSwipe.php",
+                {
+                    accept : "false"
+                },
+                function(data, status){
+                    alert("Data: " + data + "\nStatus: " + status);
+                });
+        } else if (e.keyCode == '39') {
+            // right
+        }
+    };
+</script>
 
 
 
