4987752a55432c2f68b85dbea08386a0245e7624
hlgreer <hannahgreer@rogers.com>
2017-01-28 19:46:06 -0500

    First update w/ sign up page

--- public_html/new_member.php
+++ public_html/new_member.php
@@ -1,27 +1,36 @@
 <?php
 
-var_dump($_POST);
-
-
-
-/*
 $newMemberData = array();
 
-print_r($_POST['first']);
+$newMemberData['Firstname'] = $_GET['first'];
+$newMemberData['Lastname'] = $_GET['last'];
+$newMemberData['Username'] = $_GET['username'];
+$newMemberData['Hash'] = crypt($_GET['password']);
+$newMemberData['Email'] = $_GET['email'];
+$newMemberData['Age'] = $_GET['age'];
+$newMemberData['Address'] = $_GET['address'];
+$newMemberData['Prov'] = $_GET['province'];
+$newMemberData['Postal'] = $_GET['postal'];
+$newMemberData['AnimalToAdopt'] = $_GET['pet'];
 
-$newMemberData['Firstname'] = $_POST['first'];
-$newMemberData['Lastname'] = $_POST['last'];
-$newMemberData['Username'] = $_POST['username'];
-$newMemberData['Hash'] = crypt($_POST['password']);
-$newMemberData['Email'] = $_POST['email'];
-$newMemberData['Age'] = $_POST['age'];
-$newMemberData['Address'] = $_POST['address'];
-$newMemberData['Prov'] = $_POST['province'];
-$newMemberData['Postal'] = $_POST['postal'];
-$newMemberData['AnimalToAdopt'] = $_POST['pet'];
+$json = json_encode($newMemberData);
 
-$url = 'http://robcardy.com/API/';
+$url = 'http://www.robcardy.com/api/member/';
 
 $ch = curl_init($url);
-*/
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+// Have cURL follow redirects
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERPWD, "default:password123");
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+//Execute the request
+$result = curl_exec($ch);
+echo $result;
 
