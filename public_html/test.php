e25d05821d3c8aef86df73f3c902ca4b34c069ee
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-29 01:02:39 -0500

    A bunch of stuff

--- public_html/test.php
+++ public_html/test.php
@@ -9,19 +9,19 @@ $location= "10118"; //empire state building location as default
 $code = md5($public . "key=" . $secret . "&location=10118&animal=" . $animal . "&output=" . $output . "&format=json" );
 // final processed line used to access the api
 $url = "http://api.petfinder.com/pet.getRandom?key=" . $public . "&location=10118&output=full&sig=" . $code;
+echo $url;
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'x-app-id:2979e4bf',
-    'x-app-key:4490437c2ebd3cb737a7bb8996c40ade',
-    'x-remote-user-id:0'
-));
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec($ch);
 curl_close($ch);
 
-$name = $decoded['petfinder']['pet']['options']['status'];
+print_r($data);
+
+$decoded = json_decode($data);
+
+$name = $decoded;
 print_r($name);
 print_r("XD");
