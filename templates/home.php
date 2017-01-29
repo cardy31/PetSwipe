2f0808c837a148f083e984315f847f037f5eeb90
hlgreer <hannahgreer@rogers.com>
2017-01-29 08:30:40 -0500

    Updating Home page...

--- templates/home.php
+++ templates/home.php
@@ -1,5 +1,23 @@
-<div class="jumbotron">
-    <h1>Find your new best friend!</h1>
-    <p>At Pawswipe, we specialize in pairing you with an adopted animal. Browse through hundreds of adoptable pets in your area!</p>
-    <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up Now</a></p>
+<div class="jumbotron" id="firstJum">
+    <div class="container">
+        <h1>Find your new best friend!</h1>
+        <p>At Pawswipe, we specialize in pairing you with an adopted animal. Browse through hundreds of adoptable pets in your area!</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up Now</a></p>
+    </div>
+</div>
+<div class="jumbotron jumbotron-fluid" id="secondJum">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                    <img src="https://i.reddituploads.com/cb0da287e75e47b8a4f655aa2dbd23e0?fit=max&h=1536&w=1536&s=f04b94bd428825b608eb1e0344f6f349" alt="">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                    <img src="http://i.imgur.com/SjxoKai.jpg" id="puppy1">
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
