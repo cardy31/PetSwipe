43441243685b6b57710122a529f86ceccfe5cd42
Marcus <mchan244@gmail.com>
2017-01-29 00:51:02 -0500

    yup

--- public_html/test.php
+++ public_html/test.php
@@ -1,23 +1,26 @@
 <?php
 
-$url = "http://www.robcardy.com/api/member/";
+$secret ="3b01189e7d22ef5343b93af919a2a592";
+$public= "e540e02b94af2ffb8a82381b03e11654";
+$animal= "dog"; // can be set via choice
+$output= "full"; //sets information gathered to all
+$location= "10118"; //empire state building location as default
+// creates the signature for accessing the api
+$code = md5($public . "key=" . $secret . "&location=10118&animal=" . $animal . "&output=" . $output );
+// final processed line used to access the api
+$url = "http://api.petfinder.com/pet.getRandom?key=" . $public . "&location=10118&output=full&sig=" . $code;
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'x-app-id:2979e4bf',
+    'x-app-key:4490437c2ebd3cb737a7bb8996c40ade',
+    'x-remote-user-id:0'
+));
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_USERPWD, "cardy31" . ":" . "JA83nq&E");
+curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec($ch);
 curl_close($ch);
-$data = json_decode($data, true);
-print_r($data);
-echo "<br><br>";
 
-
-print_r($data[0]);
-
-echo "<br><br>";
-
-foreach($data as $val) {
-    print_r($val);
-    echo "<br><br>";
-}
+$name = $decoded['petfinder']['pet']['options']['status']['\$t'];
+print_r($name);
