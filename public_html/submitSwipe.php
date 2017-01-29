460b14aa17c30d460fad3da7f2344feade30aac8
Rob Cardy <rob.cardy.31@gmail.com>
2017-01-29 07:15:48 -0500

    Something wrong?

--- public_html/submitSwipe.php
+++ public_html/submitSwipe.php
@@ -5,7 +5,7 @@ require("../includes/config.php");
 
 $newSwipe = array();
 $newSwipe['animalCode'] = $_POST['animalCode'];
-$newSwipe['member'] = $_SESSION['USER'];
+$newSwipe['memberUserId'] = $_SESSION['USER'];
 
 $json = json_encode($newSwipe);
 
@@ -15,7 +15,7 @@ $url = 'http://robcardy.com/api/swipedanimal/';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_USERPWD, "default" . ":" . "password123");
+curl_setopt($ch, CURLOPT_USERPWD, "cardy31" . ":" . "JA83nq&E");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $newSwipe);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
--- templates/home.php
+++ templates/home.php
@@ -1,13 +1,5 @@
-<h1>
-    PawSwipes hellow
-</h1>
-
-<button type = "button" class = "btn btn-primary" id="tosignup">Sign Up</button>
-<script>
-    $(function(){
-        $('#tosignup').click(function(){
-            window.location='signup.php'
-        });
-    });
-</script>
-
+<div class="jumbotron">
+    <h1>Find your new best friend!</h1>
+    <p>At Pawswipe, we specialize in pairing you with an adopted animal. Browse through hundreds of adoptable pets in your area!</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up Now</a></p>
+</div>
